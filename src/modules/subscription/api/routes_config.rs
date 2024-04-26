209b2f6b3f4d6effda6621e59b90aab474ab3ef8
Abraxas-365 <luisfmiranda8@gmail.com>
2024-04-25 22:23:21 -0500

    chore: post to get

--- src/modules/subscription/api/routes_config.rs
+++ src/modules/subscription/api/routes_config.rs
@@ -3,5 +3,5 @@ use actix_web::web;
 use super::handler::get_subscription;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/subscription/{user_id}").route(web::post().to(get_subscription)));
+    cfg.service(web::resource("/subscription/{user_id}").route(web::get().to(get_subscription)));
 }
